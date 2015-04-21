4cfbee794a4b16b39acb01758bb47aa90548293b
lubaochuan <lubaochuan@gmail.com>
2015-04-21 16:38:02 -0300

    finished CRUD.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,5 +17,26 @@ class MoviesController < ApplicationController
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
-  end  
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    respond_to do |client_wants|
+      client_wants.html {  redirect_to movie_path(@movie)  } # as before
+      client_wants.xml  {  render :xml => @movie.to_xml    }
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
 end
